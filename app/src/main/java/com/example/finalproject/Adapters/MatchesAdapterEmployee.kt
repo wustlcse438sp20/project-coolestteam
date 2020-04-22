f4412da32f0d0ff276ac4919045c3adfbe0364bc
Joe Frazier <frazierj@wustl.edu>
2020-04-21 22:37:35 -0500

    Update

--- app/src/main/java/com/example/finalproject/Adapters/MatchesAdapterEmployee.kt
+++ app/src/main/java/com/example/finalproject/Adapters/MatchesAdapterEmployee.kt
@@ -1,7 +1,6 @@
 package com.example.finalproject.Adapters
 
-import android.content.Intent
-import android.util.Log
+
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.LinearLayout
@@ -9,7 +8,6 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.finalproject.Data.PostMatch
-import com.example.finalproject.Data.Posting
 import com.example.finalproject.R
 
 //create the view holder
