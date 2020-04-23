c46c6ddd608b6bb2d1dc2aa93094efb6ee63d741
Jacob Gilhaus <jacob.gilhaus16@kzoo.edu>
2020-04-22 23:32:09 -0500

    Employer Profile Page

--- app/src/main/java/com/example/finalproject/Activities/EmployerUI/ProfileEmployerActivity.kt
+++ app/src/main/java/com/example/finalproject/Activities/EmployerUI/ProfileEmployerActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.ImageButton
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -14,7 +15,12 @@ import com.example.finalproject.Adapters.PostingsListAdapter
 import com.example.finalproject.R
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_home_employee.*
 import kotlinx.android.synthetic.main.activity_profile_employer.*
+import kotlinx.android.synthetic.main.activity_profile_employer.home_button
+import kotlinx.android.synthetic.main.activity_profile_employer.logout_button
+import kotlinx.android.synthetic.main.activity_profile_employer.match_button
+import kotlinx.android.synthetic.main.activity_profile_employer.profile_button
 
 class ProfileEmployerActivity : AppCompatActivity() {
     private lateinit var logoutButton: ImageButton
@@ -22,6 +28,11 @@ class ProfileEmployerActivity : AppCompatActivity() {
     private lateinit var createJobButton: ImageButton
     private lateinit var matchButton: ImageButton
     private lateinit var homeButton: ImageButton
+
+    private lateinit var company: TextView
+    private lateinit var liason: TextView
+    private lateinit var contact: TextView
+    private lateinit var profile: TextView
 
     lateinit var postingList: MutableList<String>
     lateinit var recyclerView: RecyclerView
@@ -64,13 +75,25 @@ class ProfileEmployerActivity : AppCompatActivity() {
             )
         }
 
+        //text for textviews
+        company = employer_text
+        liason = liason_text
+        profile = profile_text
+        contact = contact_text
+
+        profile.text = "Employer Profile"
+
         var db = FirebaseFirestore.getInstance()
         var auth = FirebaseAuth.getInstance()
         postingList = arrayListOf()
 
         var doc = db.collection("Employers").document(auth.currentUser!!.uid)
         doc.get().addOnSuccessListener { document ->
-            Log.d("here", "${document.data!!.get("company").toString()}")
+            company.text = "Employer: " + document.data!!.get("company").toString()
+            liason.text  = "Liason: " + document.data!!.get("name").toString()
+            contact.text  = "Contact: " + document.data!!.get("email").toString()
+
+                    Log.d("here", "${document.data!!.get("company").toString()}")
             //var companyName = document.data!!.get("company").toString()
             doc.collection("Postings").get()
                 .addOnSuccessListener { result ->
